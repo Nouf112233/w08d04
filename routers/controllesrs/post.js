dc13a72d41304a50e2f0089c7a506f88b214bb09
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-01 13:34:10 +0300

    create getPoste

--- routers/controllesrs/post.js
+++ routers/controllesrs/post.js
@@ -31,8 +31,33 @@ const userModel = require("./../../db/models/user");
       });
   };
 
+  const getPosts = (req, res) => {
+    const { id } = req.body;
+  
+    userModel
+      .find({ _id: id })
+      .then((result) => {
+        postModel
+          .find({$and: [{user: id}, {isdeleted: false}] })
+          .then((result) => {
+              if(result)
+              {
+                res.status(200).json(result);
+              }else{
+                res.status(400).json("User not has any posts");
+              }   
+          })
+          .catch((err) => {
+            res.status(400).json("User not has any posts");
+          });
+      })
+      .catch((err) => {
+        res.status(400).json("User not found");
+      });
+  };
+
   
 
 
 
-module.exports = {createPost };
+module.exports = {createPost,getPosts };
--- routers/routes/post.js
+++ routers/routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createPost } = require("./../controllesrs/post");
+const { createPost,getPosts } = require("./../controllesrs/post");
 const postRouter = express.Router();
 const authontication=require("./../middelwares/authontication");
 const authorization=require("./../middelwares/authorization");
 
 postRouter.post("/post",createPost);
+postRouter.post("/getpost",getPosts);
 
 
 module.exports = postRouter;
