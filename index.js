5bda942fdbab089727ce1ffbb2b833dd59500356
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-01 11:17:53 +0300

    create index.js file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// require("./db/index");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// const roleRouter = require("./routers/routes/role");
+// app.use("/role", roleRouter);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
