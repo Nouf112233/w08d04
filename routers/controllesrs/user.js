f8a251a930e5de87e905f156baf579742095cf0b
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-01 11:58:30 +0300

    create login

--- routers/controllesrs/user.js
+++ routers/controllesrs/user.js
@@ -27,7 +27,38 @@ const register = async (req, res) => {
     });
 };
 
+const login = (req, res) => {
+    const { name, password } = req.body;
+  
+    const savedname = name.toLowerCase();
+  
+    userModel
+      .findOne({$or: [{email: savedname},{username: savedname}] })
+      .then(async (result) => {
+        if (result)
+         {
+            const hashedPass = await bcrypt.compare(password, result.password);
+            if (hashedPass) 
+            {
+              const payload = {
+                role: result.role,
+              };
+              const options = { expiresIn: "60m" };
+              const token = await jwt.sign(payload, secret, options);
+              res.status(200).json({ result, token });
+            } else {
+              res.status(400).json("invalid email or password");
+            }
+          
+        } else {
+          res.status(404).json("user does not exit");
+        }
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  };
+  
+  
 
-
-
-module.exports = { register};
+module.exports = { register,login};
--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register } = require("./../controllesrs/user");
+const { register,login } = require("./../controllesrs/user");
 const userRouter = express.Router();
 const authontication=require("./../middelwares/authontication");
 const authorization=require("./../middelwares/authorization");
 
 userRouter.post("/register", register);
+userRouter.post("/login", login);
 
 
 module.exports = userRouter;
