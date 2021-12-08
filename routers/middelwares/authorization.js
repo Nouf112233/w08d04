b35a2585324b6d7151aa5b063d4f15bbae6f907d
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-08 13:15:54 +0300

    add

--- routers/middelwares/authorization.js
+++ routers/middelwares/authorization.js
@@ -1,4 +1,8 @@
 const roleModel =require("./../../db/models/role");
+const userModel = require("./../../db/models/user");
+const postModel = require("./../../db/models/post");
+const commentModel = require("./../../db/models/comment");
+
 
 const adminAuthorization=async (req,res,next)=>{
     try{
@@ -16,6 +20,7 @@ const adminAuthorization=async (req,res,next)=>{
     }
 }
 
+
 const userAuthorization=async (req,res,next)=>{
     try{
         const tokenId=req.token.id;
@@ -24,11 +29,63 @@ const userAuthorization=async (req,res,next)=>{
         {
             next();
         }else{
-            return res.status(403).json({message:"forbidden"});
+            return res.status(403).json({message:"admin forbidden"});
         }
 
     }catch{
         res.status(403).json(error);
     }
 }
+// const userAuthorization=async (req,res,next)=>{
+//     try{
+//         const tokenId=req.token.id;
+//         const result=await userModel.findById(tokenId);
+        
+//         if(result&&result.isdeleted==false)
+//         {
+//             next();
+//         }else{
+//             return res.status(403).json({message:"user forbidden"});
+//         }
+
+//     }catch{
+//         res.status(403).json(error);
+//     }
+// }
+
+// const postAuthorization=async (req,res,next)=>{
+//         try{
+//             const tokenId=req.token.id;
+//             const result=await postModel.findById(req.body.postId);
+            
+//             if(result&&result.isdeleted==false&&result.user===tokenId)
+//             {
+//                 next();
+//             }else{
+//                 return res.status(403).json({message:"post forbidden"});
+//             }
+    
+//         }catch{
+//             res.status(403).json(error);
+//         }
+//     }
+
+    // const commentAuthorization=async (req,res,next)=>{
+    //     try{
+    //         const tokenId=req.token.id;
+    //         const result=await commentModel.findById(req.body.commentId);
+            
+    //         if(result&&result.isdeleted==false&&result.user===tokenId)
+    //         {
+    //             next();
+    //         }else{
+    //             return res.status(403).json({message:"comment forbidden"});
+    //         }
+    
+    //     }catch{
+    //         res.status(403).json(error);
+    //     }
+    // }
+
+
 module.exports={adminAuthorization,userAuthorization};
